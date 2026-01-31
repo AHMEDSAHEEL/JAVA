266fb6f1955c081d8516a6059e6c57f954c80004
Ahmed Saheel S <shaheel123as@gmail.com>
2026-01-31 10:03:51 +0530

    formatted the code

--- java basics/duplicate.java
+++ java basics/duplicate.java
@@ -2,36 +2,35 @@ import java.util.*;
 
 public class duplicate {
     public static void main(String[] args) {
-        String str="Ahmed Saheel";
-        TreeMap<Character,Integer>hm=new TreeMap<>();
+        String str = "Ahmed Saheel";
+        TreeMap<Character, Integer> hm = new TreeMap<>();
 
-        for(char c: str.toCharArray()){
-            if(hm.containsKey(c)){
-                hm.put(c,hm.get(c)+1);
-            }else{
-                hm.put(c,1);
+        for (char c : str.toCharArray()) {
+            if (hm.containsKey(c)) {
+                hm.put(c, hm.get(c) + 1);
+            } else {
+                hm.put(c, 1);
             }
         }
-   //System.out.println(hm);
-    
-   //first Method: 
+        // System.out.println(hm);
 
-            //    for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
-            //     System.out.println("Character: " + entry.getKey() + ", Count: " + entry.getValue());
-            // }
+        // first Method:
 
+        // for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+        // System.out.println("Character: " + entry.getKey() + ", Count: " +
+        // entry.getValue());
+        // }
 
-  //second Method: 
+        // second Method:
         // Iterator<Map.Entry<Character,Integer>> entry=hm.entrySet().iterator();
 
         // while(entry.hasNext()){
-        //     Map.Entry<Character,Integer> iter=entry.next();
-        //     System.out.println(iter.getKey()+" "+iter.getValue());
+        // Map.Entry<Character,Integer> iter=entry.next();
+        // System.out.println(iter.getKey()+" "+iter.getValue());
         // }
 
-
-  //third Method: 
-         hm.forEach((key, value) -> System.out.println(key + " " + value));
+        // third Method:
+        hm.forEach((key, value) -> System.out.println(key + " " + value));
 
     }
 }
