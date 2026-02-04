77daa778e06b0bb4ea283d97a41dfeb223943c7b
Ahmed Saheel S <shaheel123as@gmail.com>
2026-02-04 22:33:43 +0530

    added numbers palindrome

--- java basics/palindorme.java
+++ java basics/palindorme.java
@@ -2,14 +2,26 @@ public class palindorme {
     public static void main(String[] args) {
         String orgStr="mam";
         String str="";
-        for(int i=0;i<orgStr.length();i++){
-            str=orgStr.charAt(i)+str;
+int x=-121;
+          int n=x,rev=0;
+        while(n>0){
+            int rem=n%10;
+            rev=(rev*10)+rem;
+            n=n/10;
         }
-
-        if ((orgStr.equals(str))) {
+         if(x==rev){
             System.out.println("true");
         }else{
             System.out.println("false");
         }
+        // for(int i=0;i<orgStr.length();i++){
+        //     str=orgStr.charAt(i)+str;
+        // }
+
+        // if ((orgStr.equals(str))) {
+        //     System.out.println("true");
+        // }else{
+        //     System.out.println("false");
+        // }
     }
 }
