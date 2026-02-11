eec6ec60a8869c1f2b302761797f68bf496983e5
Ahmed Saheel S <shaheel123as@gmail.com>
2026-02-11 23:11:09 +0530

    pattern

--- DSA/Patterns/pattern_1.java
+++ DSA/Patterns/pattern_1.java
@@ -6,9 +6,14 @@ public class pattern_1 {
          Scanner s=new Scanner(System.in);
         do{
         int n=s.nextInt();
-        //pattern1(n);
-         //pattern_2(n);
-         pattern_6(n);
+        // pattern1(n);
+        //  pattern_2(n);
+        //  pattern_3(n);
+        //  pattern_4(n);
+        //  pattern_5(n);
+        //  pattern_6(n);
+        pattern_7(n);
+        pattern_8(n);
         System.out.println("Do you want to Continue the Pattern: ");
          ans=s.next().charAt(0);
         }while(ans=='y');
@@ -50,6 +55,69 @@ public class pattern_1 {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i+1;j++){
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern_7(int n){
+        /*
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+        */
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=num;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+                num=num+2;
+
+            System.out.println();
+        }
+    }
+    public static void pattern_8(int n){
+        /*
+    *    
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+        */
+       int num = 1;
+        int index = 0;
+        for (int i = 1; i <= n*2-1; i++) {
+            if (i > n) {
+                index = i-n;
+
+            } else {
+                index = n - i;
+            }
+            for (int j = 1; j <= index; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= num; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= index; j++) {
+                System.out.print(" ");
+            }
+            if (i >= n) {
+                num = num - 2;
+
+            } else {
+                num = num + 2;
             }
             System.out.println();
         }
